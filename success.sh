cf5e5d4e227ad0a22520ffd6f5e14036adfe7d44
Brando <--global>
2023-10-24 10:22:59 +1100

    Adding ubuntu support

--- success.sh
+++ success.sh
@@ -7,7 +7,19 @@
 declare -a success_messages=("Presto" "yeah bay-bee!" "Thatz showbizz" "Ooh yeah" "Perfecto" "Deeee-lish-us!" "Deeee-lish" "That'za wrap" "Bada-bing, bada-booom!" "Zippedy-doo-dah!" "Ayyyyy!" "Bingo" "bango" "Hot diggity dog")
 
 # Generate a random index
-index=$((RANDOM % ${#success_messages[@]}))
+message="${success_messages[$((RANDOM % ${#success_messages[@]} + 1))]}"
+
+# Determine the OS type
+OS=$(uname)
 
 # Use the say command with a randomly selected success message
-say "${success_messages[$index]}"
+
+if [[ "$OS" == "Darwin" ]]; then
+  # macOS
+  say $message
+elif [[ "$OS" == "Linux" ]]; then
+  # assuming ubuntu for linux
+  espeak $message
+fi
+
+echo $message
